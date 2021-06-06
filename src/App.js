bd71b2ad6775ef96f2fe143b884eff62e371d825
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-06 23:33:21 +0530

    Fetching First Data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div className="App">
       <h2>Hello Ignite</h2>
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -7,14 +7,10 @@ const initState = {
 const gamesReducer = (state = { initState }, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state };
+      return { ...state, popular: action.payload.popular };
     default:
       return { ...state };
   }
 };
-const fetchGames = (userData) => {
-  return {
-    type: "FETCH_GAMES",
-  };
-};
+
 export default gamesReducer;
