83fba7e8b80dae6163c73538e03e25fac87ed3b9
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-09 19:39:32 +0530

    Global Styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
